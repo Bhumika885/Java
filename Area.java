bb45655ff9f78f81117997a4cc69813778fd3256
Bhumika Rathore <bhumika11xworkz@gmail.com>
2024-07-22 10:41:45 +0530

    java

---
+++ Area.java
@@ -0,0 +1,14 @@
+class Area
+{
+	public static void main(String[]args)
+	{
+		String car="porsche";
+		car="red";
+		String bike="bullet";
+		String store=car+bike;
+		System.out.println("store:"+store);
+		
+		
+		
+	}
+}
