d39c455f37513169181c6557c488d2a890de92b8
Bhumika Rathore <bhumika11xworkz@gmail.com>
2024-08-06 21:29:05 +0530

    java

---
+++ Projector.java
@@ -0,0 +1,17 @@
+class Projector { 
+
+
+    String company;
+    String type;
+    String colour;
+    double weight;
+
+    
+  Projector(String company,String type,String colour,double weight) {
+       
+        this.company = brand;
+        this.type = type;
+        this.colour = colour;
+        this.weight = weight;
+    }
+ }
---
+++ ProjectorRunner.java
@@ -0,0 +1,37 @@
+class ProjectorRunner{
+
+public static void main(String[]args){
+	
+	
+	
+	
+	    Projector projector1 = new Projector("hp", "type", "orange",100);
+		System.out.println("projectorname:"+projector1.name);
+		System.out.println("projectortype":"+projector1.type);
+		System.out.println("projectorcolour:"+projector1.colour);
+		System.out.println("projectorweight:"+projector1.weight);
+		
+		
+        Projector projector2 = new Projector("hp", "type", "orange",100);
+		System.out.println("projectorname:"+projector2.name);
+		System.out.println("projectortype":"+projector2.type);
+		System.out.println("projectorcolour:"+projector2.colour);
+		System.out.println("projectorweight:"+projector2.weight);
+		
+		
+		Projector projector3= new Projector("hp", "type", "orange",100);
+		System.out.println("projectorname:"+projector3.name);
+		System.out.println("projectortype":"+projector3.type);
+		System.out.println("projectorcolour:"+projector3.colour);
+		System.out.println("projectorweight:"+projector3.weight);
+		
+		 	
+
+
+        
+        
+
+}
+}
+
+
