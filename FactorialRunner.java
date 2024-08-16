f33fea2927fd0eea3b9ce2e143d99e5a89c8c3b4
Bhumika Rathore <bhumika11xworkz@gmail.com>
2024-08-16 14:22:38 +0530

    java

---
+++ FactorialRunner.java
@@ -0,0 +1,7 @@
+    public static void main(String[] args) 
+    { 
+        int num = 5; 
+        System.out.println("Factorial of " + num + " is "
+                           + factorial(5)); 
+    } 
+}
